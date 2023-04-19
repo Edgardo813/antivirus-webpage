d8cd1d464a678b3e899d314d70f66007844175bb
Edgard <edgard.david179@gmail.com>
2023-04-19 15:58:50 -0400

    Navbar added

--- src/pages/components/Navbar.tsx
+++ src/pages/components/Navbar.tsx
@@ -1,9 +1,33 @@
 import React from 'react';
-
+import Link from 'next/link';
+import { FaBars, FaTimes } from 'react-icons/fa';
 const Navbar = () => {
   return (
-    <div className='container mx-auto'>
-      <nav>Navbar</nav>
+    <div className='container'>
+      <nav className='z-40 sticky top-0 w-full'>
+        <div className='flex items-center min-h-[6rem] p-[0.5rem] text-xl text-black'>
+          <div className='flex-1 place-content-center md:place-content-start flex  ml-8'>
+            Logo
+          </div>
+
+          <div>
+            <ul className='lg:flex gap-24 hidden justify-around'>
+              <li>
+                <Link href='/'>Acerca de Nosotros </Link>
+              </li>
+              <li>
+                <Link href='/'>Servicios </Link>
+              </li>
+              <li>
+                <Link href='/'>Galeria</Link>
+              </li>
+              <li>
+                <Link href='/'>Contacto</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </div>
   );
 };
